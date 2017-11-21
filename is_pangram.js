a0cfdeec5340d97ee5b15c7a3c6409cc589e30b2
Pietro Martini <pmartini@bermangrp.com>
2017-11-21 17:28:46 -0500

    remove regex for uppercase

--- is_pangram.js
+++ is_pangram.js
@@ -1,4 +1,4 @@
-const isPangram = string => string.toLowerCase().replace(/[^a-zA-Z]+/g, '').split('').reduce((arr, char) => {
+const isPangram = string => string.toLowerCase().replace(/[^a-z]+/g, '').split('').reduce((arr, char) => {
 	if ( arr.indexOf(char) === -1 ) arr.push(char);
 	return arr;
 }, []).length === 26;
